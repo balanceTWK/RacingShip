25efcb928c61b42c9f8b1ef6aac974329842cef1
balanceTWK <linuxtwk@outlook.com>
2018-07-24 10:09:56 +0800

    add mpu9250 attitude algorithm. (first version)

--- Ship/rt-thread3.1.0/bsp/stm32f10x/drivers/read_io_port.c
+++ Ship/rt-thread3.1.0/bsp/stm32f10x/drivers/read_io_port.c
@@ -7,7 +7,7 @@ void  rt_hw_portDEFG_init(void)
 {
     GPIO_InitTypeDef GPIO_InitStructure;
 
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOD|RCC_APB2Periph_GPIOE|RCC_APB2Periph_GPIOF|RCC_APB2Periph_GPIOG,ENABLE);
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOD | RCC_APB2Periph_GPIOE | RCC_APB2Periph_GPIOF | RCC_APB2Periph_GPIOG, ENABLE);
 
     GPIO_InitStructure.GPIO_Mode  = GPIO_Mode_IPD;
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
